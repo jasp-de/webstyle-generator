c325a672860b115eefc0f9350581a6f4265b35d0
jasp_de <d.j.deppermann@gmail.com>
2024-11-08 15:32:32 +0100

    padding etc

--- src/app/components/StyleCard.js
+++ src/app/components/StyleCard.js
@@ -6,7 +6,9 @@ export default function StyleCard({ style }) {
       <div className="style-frame" style={css.frame}>
         <h1 style={css.h1}>{text.title}</h1>
         <p style={css.p}>{text.shortDescription}</p>
-        <button style={css.button}>{text.buttonText}</button>
+        <button className="preview-button" style={css.button}>
+          {text.buttonText}
+        </button>
       </div>
       <div className="style-info">
         <div className="style-name">{info.name}</div>
